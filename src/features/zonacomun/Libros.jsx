215e609b31e38b6e041c8f11cd7800e41d05151d
maragubit <maragubit@gmail.com>
2025-11-13 19:19:27 +0100

    title books

--- src/features/zonacomun/Libros.jsx
+++ src/features/zonacomun/Libros.jsx
@@ -3,14 +3,14 @@ import book from "../../assets/images/book.png";
 import { Link } from "react-router-dom";
 function Libros({libros}) {
   return(<>
-  <Row className="g-3">
+  <Row className="g-2 align-items-stretch mt-3 mb-5">
   {libros?.map(libro => (
 
-            <Col xs="12" md="6" lg="4" key={libro.id} className="d-flex justify-content-center  p-auto">
+            <Col xs="12" md="6" lg="4" key={libro.id} className="justify-content-center">
             <Link to={libro.file} target="_blank" style={{ textDecoration: 'none', color: 'inherit' }}>
-            <Card key={libro.id} className="mb-3 border-0">
+            <Card key={libro.id} className="mb-3 border-0 h-100 ">
             
-            <Card.Title>{libro.title}</Card.Title>
+            <Card.Title style={{minHeight: '3rem'}}>{libro.title}</Card.Title>
             <Card.Img className="justify-content-center mx-auto" src={book} alt={libro.title} style={{ height: '300px', width: '190px' }} />
             <Card.Text>{libro.author}</Card.Text>
             <Card.Footer className="text-muted"> {libro.course.title}</Card.Footer>
